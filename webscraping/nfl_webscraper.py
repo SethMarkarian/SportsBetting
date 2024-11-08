b1b74be7242e37d2d9e3e6de98ae8813654b8a74
Seth Markarian <seth.h.markarian@gmail.com>
2024-11-08 17:56:32 -0500

    🐞 fix: scraperV1.5- fix nfl
    fixed nfl data by adding insights and linemate percentage

--- webscraping/nfl_webscraper.py
+++ webscraping/nfl_webscraper.py
@@ -12,24 +12,47 @@ driver.get('https://linemate.io/nfl/trends')
 time.sleep(5)  # Adjust based on your internet speed
 
 # Extract the player data
-items = driver.find_elements(By.CLASS_NAME, 'trend-content-header-info')
+items = driver.find_elements(By.CLASS_NAME, 'trend-content-header')
 players = []
 for item in items:
-    player_name = item.find_element(By.CLASS_NAME, 'text-style-body-medium').text
-    matchup = item.find_element(By.CLASS_NAME, 'text-style-caption-uppercase-normal').text
-    market = item.find_element(By.CLASS_NAME, 'text-style-label-medium').text
-    odds = item.find_element(By.CLASS_NAME, 'text-style-label-semibold').text
-    players.append([player_name, matchup, market, odds])
+    try:
+        player_name = item.find_element(By.CLASS_NAME, 'text-style-body-medium').text
+        matchup = item.find_element(By.CLASS_NAME, 'text-style-caption-uppercase-normal').text
+        market = item.find_element(By.CLASS_NAME, 'text-style-label-medium').text
+        odds = item.find_element(By.CLASS_NAME, 'text-style-label-semibold').text
+
+        players.append([player_name, matchup, market, odds])
+    except Exception as e:
+        print(f"Error extracting data from an item: {e}")
+        continue
+
+# Extract the insight data
+items = driver.find_elements(By.CLASS_NAME, 'trend-content-insights')
+insights = []
+
+for item in items:
+    try:
+        insight = item.find_element(By.CLASS_NAME, 'text-style-label-normal').text.replace("• ", "")
+        linemate_percentage = item.find_element(By.CLASS_NAME, 'text-style-label-tabular-medium').text
+
+        insights.append([insight, linemate_percentage])
+    except Exception as e:
+        print(f"Error extracting data from an item: {e}")
+        continue
+
 
 # Get current date
 current_date = date.today()
 
+# Combine player data with linemate insights
+combined_player_insight_data = [players + insights for players, insights in zip(players, insights)]
+
 # Save data to CSV
 with open(f'../nfl_saved_csv/nfl_betting_data_{current_date.strftime("%m_%d_%Y")}.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['Player Name', 'Matchup', 'Market', 'Odds'])
-    writer.writerows(players)
+    writer.writerow(['Player Name', 'Matchup', 'Market', 'Odds', 'Trend Context', 'Linemate Percentage'])
+    writer.writerows(combined_player_insight_data)
 
-print('Data has been saved to ../saved_csv/nfl_betting_data.csv')
+print('Data has been saved to ../nfl_saved_csv/nfl_betting_data.csv')
 
 driver.quit()
