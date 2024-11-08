6a7775d626a40f3e04197f29f7796b26168be88b
Seth Markarian <seth.h.markarian@gmail.com>
2024-11-07 19:53:37 -0500

    ✨ feat: webscraping v1.0
    added webscraper v1 for nba and nfl and turning them into csv files

---
+++ webscraping/nfl_webscraper.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import csv
+from datetime import date
+
+# Set up the WebDriver
+driver = webdriver.Chrome()  # Ensure ChromeDriver is installed and in PATH
+driver.get('https://linemate.io/nfl/trends')
+
+# Wait for the page to load JavaScript content
+time.sleep(5)  # Adjust based on your internet speed
+
+# Extract the player data
+items = driver.find_elements(By.CLASS_NAME, 'trend-content-header-info')
+players = []
+for item in items:
+    player_name = item.find_element(By.CLASS_NAME, 'text-style-body-medium').text
+    matchup = item.find_element(By.CLASS_NAME, 'text-style-caption-uppercase-normal').text
+    market = item.find_element(By.CLASS_NAME, 'text-style-label-medium').text
+    odds = item.find_element(By.CLASS_NAME, 'text-style-label-semibold').text
+    players.append([player_name, matchup, market, odds])
+
+# Get current date
+current_date = date.today()
+
+# Save data to CSV
+with open(f'../nfl_saved_csv/nfl_betting_data_{current_date.strftime("%m_%d_%Y")}.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Player Name', 'Matchup', 'Market', 'Odds'])
+    writer.writerows(players)
+
+print('Data has been saved to ../saved_csv/nfl_betting_data.csv')
+
+driver.quit()
