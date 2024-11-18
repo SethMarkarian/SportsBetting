507f6e4916edf34f51030aca448573cc2b8621f0
Seth Markarian <seth.h.markarian@gmail.com>
2024-11-18 13:58:37 -0500

    ↩ revert: reverting

--- webscraping/nba_webscraper.py
+++ webscraping/nba_webscraper.py
@@ -6,21 +6,8 @@ import time
 import csv
 from datetime import date
 
-# Set up Chrome options for headless mode and CI/CD compatibility
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--remote-debugging-port=9222")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.binary_location = '/usr/bin/google-chrome'
-
-# Set up ChromeDriver path
-chrome_driver_path = '/usr/local/bin/chromedriver'
-service = Service(chrome_driver_path)
-
 # Initialize the WebDriver
-driver = webdriver.Chrome(service=service, options=chrome_options)
+driver = webdriver.Chrome()
 
 # Navigate to the web page
 driver.get('https://linemate.io/nba/trends')
